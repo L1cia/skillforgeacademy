8a7c180f914d330aae2b3137f969306e9c0c968c
Ifedolapo <oluwafunmike222@gmail.com>
2025-09-06 14:07:02 +0100

    Fix mobile menu and add login/signup buttons

--- landing.js
+++ landing.js
@@ -12,12 +12,21 @@
     observers.forEach(el => observer.observe(el));
 
       
+
     const menuBtn = document.getElementById("menu-btn");
     const menu = document.getElementById("menu");
+
     menuBtn.addEventListener("click", () => {
       menu.classList.toggle("hidden");
     });
 
+
+    document.querySelectorAll("#menu a").forEach(link => {
+      link.addEventListener("click", () => {
+        menu.classList.add("hidden");
+      });
+    });
+  
 document.querySelectorAll("nav a").forEach(link => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
